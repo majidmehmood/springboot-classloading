caafe333d98d6071b12e7ebc5f1206202b567ea1
Mehmood,Majid (init) <Majid.Mehmood@init.de>
2022-07-12 23:14:41 +0200

    added basic documentation

---
+++ src/main/java/com/majid/classloading/Service.java
@@ -0,0 +1,12 @@
+package com.majid.jwt_article;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Service {
+    public static void main(String[] args) {
+        SpringApplication.run(Service.class, args);
+    }
+}
